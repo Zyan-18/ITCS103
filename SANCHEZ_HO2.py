dce680ea3673b7201a428588144dc5ddf996d848
Zyan-18 <sanchezzyankloe@gmail.com>
2026-02-10 15:17:46 +0800

    Update SANCHEZ_HO2.py

--- SANCHEZ_HO2.py
+++ SANCHEZ_HO2.py
@@ -13,27 +13,27 @@ label = tk.Label(window, text="Student Profile",
     bg = "pink",
     anchor = "center")
 
-label_1 = tk.Label(window, text="Name: Zyan Kloe S. Sanchez",
+label_1 = tk.Label(window, text="Name : Zyan Kloe S. Sanchez",
     font = ("arial", 18),
     fg = "brown",
     bg = "pink")
 
-label_2 = tk.Label(window, text="Age: 19",
+label_2 = tk.Label(window, text="Age : 19",
     font = ("arial", 18),
     fg = "brown",
     bg = "pink")
 
-label_3 = tk.Label(window, text="Course and Section: BSIT-1A",
+label_3 = tk.Label(window, text="Course : BSIT-1A",
     font = ("arial", 18),
     fg = "brown",
     bg = "pink")
 
-label_4 = tk.Label(window, text="Birthday: November 20, 2006",
+label_4 = tk.Label(window, text="Birthday : November 20, 2006",
     font = ("arial", 18),
     fg = "brown",
     bg = "pink")
 
-label_5 = tk.Label(window, text="Motto: Treat yourself as kindly as you treat others.",
+label_5 = tk.Label(window, text="Motto : Treat yourself as kindly as you treat others.",
     font = ("arial", 18),
     fg = "brown",
     bg = "pink")
@@ -46,4 +46,5 @@ label_4.pack(padx=20, pady=6, anchor = "w")
 label_5.pack(padx=20, pady=6, anchor = "w")
 
 window.mainloop()
+
 
